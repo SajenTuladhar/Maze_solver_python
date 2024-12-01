4f1e78a27e9bd31f202c9ba78b98f611218f7a89
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-12-01 15:17:40 +0545

    upload image button

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import tkinter  as tk  #tk as alias
+from tkinter import filedialog, messagebox
 from tkinter import ttk  #ttk themed tkinter widgets
 import pygame
 
         
 
 class MazeApp:
-    def __init__(self): # constructor method< initialzies class , is automatically called>
+    def __init__(self,root): # constructor method< initialzies class , is automatically called>
          #everything in the GUI is in between this :start
-        self.root = tk.Tk() #creates the main GUI window
+        self.root = root #creates the main GUI window
+        self.root.title("Maze Solver")
+        self.canvas_width= 600  
+        self.canvas_height= 400 
+        self.gridsize = 20
+        
+        #buttons
+        tk.Button(root, text="Upload Maze", command= self.upload_maze).pack(pady=5)
         
         
+        
+    def upload_maze(self):
+         filepath = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
+         if filepath:
+            self.process_image(filepath)
         
         
         #and this :end 
-        self.root.mainloop() # keeps the GUI open until the user closes it 
+            self.root.mainloop() # keeps the GUI open until the user closes it 
         
     
     
 
 if __name__ =="__main__": #ensures the following code runs only if the script is executed directly (not imported as module)
-    MazeApp() 
+    root = tk.Tk()
+    app= MazeApp(root)
+    root.mainloop() 
