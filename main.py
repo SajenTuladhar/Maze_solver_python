d80478f56e9e956f8ae9ec0f7fbccd19488b998c
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-12-02 12:46:12 +0545

    add frame for buttons and empty functions

--- main.py
+++ main.py
@@ -14,8 +14,18 @@ class MazeApp:
         self.canvas_height= 400 
         self.gridsize = 20
         
+        
+        button_frame = tk.Frame(root)
+        button_frame.pack(pady=10)
+        button_frame.pack(side="bottom", pady=10)
+
         #buttons
-        tk.Button(root, text="Upload Maze", command= self.upload_maze).pack(pady=5)
+        tk.Button(button_frame, text="Upload Maze", command=self.upload_maze).grid(row=0, column=0, padx=5)
+        tk.Button(button_frame, text="Draw Maze", command=self.draw_maze).grid(row=0, column=1, padx=5)
+        tk.Button(button_frame, text="Solve Maze", command=self.solve_maze).grid(row=0, column=2, padx=5)
+        tk.Button(button_frame, text="Clear", command=self.clear_canvas).grid(row=0, column=3, padx=5)
+
+        
         
         
         
@@ -23,10 +33,19 @@ class MazeApp:
          filepath = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
          if filepath:
             self.process_image(filepath)
+    
+    def draw_maze(self):
+        pass
+
+    def solve_maze(self):
+        pass
+
+    def clear_canvas(self):
+        self.canvas.delete("all")
         
         
         #and this :end 
-            self.root.mainloop() # keeps the GUI open until the user closes it 
+        self.root.mainloop() # keeps the GUI open until the user closes it 
         
     
     
